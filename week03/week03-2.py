7865db9fe7d4328b87f992983fe6a51b8e641caf
xiandong1 <hg96041057@gmail.com>
2024-03-06 10:45:03 +0800

    week03

---
+++ week03/week03-2.py
@@ -0,0 +1,9 @@
+#week03-2 python
+
+
+print("請輸入兩個數字")
+a , b = list(map(int,input().split()))
+print(f'{a:5}')
+print(f'{b:5}')
+print('------------')
+print(f'{a+b:5}')
---
+++ week03/week03-3.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+
+    int a = 10;
+    printf("%5d占了五格\n",a);
+    float pi = 3.141592653589793238462643383279;
+    printf("%f 浮點數只有8格\n",pi);
+    printf("%12f 如果印12格\n",pi);
+    printf("%12.10f 印12格,點的右邊有10格\n",pi);
+    double pi2=3.141592653589793238462643383279;
+    printf("%12.10f 兩倍的浮點數double印點右邊10格\n",pi2);
+
+}
