0032cad3a9eaacfe472e05931f6fd73359e821d4
xiandong1 <hg96041057@gmail.com>
2024-03-13 10:51:20 +0800

    week04

---
+++ week04/week04-4.cpp
@@ -0,0 +1,21 @@
+///修改至week04-3.cpp
+#include <stdio.h>
+int main()
+{
+
+    int a = 3;
+    while(a>0)
+    {
+
+        printf("a是%d\n",a);
+        a--; ///a=a-1
+
+    };
+
+    for(int b = 3;b>0;b--){
+
+        printf("b是%d\n",b);
+
+    };///for迴圈精簡有力
+
+}
