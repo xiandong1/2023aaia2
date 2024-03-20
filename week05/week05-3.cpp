18efd0c12866d84d17ae82262c739189319acb93
xiandong1 <hg96041057@gmail.com>
2024-03-20 10:43:54 +0800

    week05

---
+++ week05/week05-3.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main(){
+
+    for(int i=0; i<9; i++){///基礎型從0開始
+
+        printf("i is %d ",i);
+
+    };
+
+    printf("\n");
+
+    for(int i=1; i<=9; i++){///進階型從1開始
+
+        printf("i is %d ",i);
+
+    };
+
+    printf("\n");
+
+    for(int i=9; i>=0;i--){
+
+        printf("i is %d ",i);
+
+    };
+
+}
